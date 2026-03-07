e3ae4cfeedf6bcecc6a1807d31412a9c95ba6258
Cdm2883 <Cdm2883@hotmail.com>
2026-03-07 22:22:59 +0800

    feat: simple port of LeviLamina hook macros

--- include/Mortis/Mortis.hpp
+++ include/Mortis/Mortis.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Mortis/Config.hpp>
+#include <Mortis/HookMacro.hpp>
 #include <Mortis/ImportHook.hpp>
 #include <Mortis/InlineHook.hpp>
 #include <Mortis/MemoryScanner.hpp>
